5c182db84802254a43b1edd4f9a95b3df7cb9719
Ninny <neninny@proton.me>
2026-01-26 15:48:47 -0500

    fixed the unlock method

--- main.py
+++ main.py
@@ -9,21 +9,20 @@ class PasswordVault:
         if password_attempt == self.__master_password:
             self.__is_locked = False
             self.__failed_attempts = 0
-        elif self.__failed_attempts >= 3:
-            print("Vault locked permanently!")
         else:
             self.__failed_attempts += 1
             print(f"ERROR: Incorrect password. You have {self.__failed_attempts}/3 attempts left.")
-        
-        print(f"DEBUG: Is vault locked?: {self.__is_locked}")
-        print(f"DEBUG: How many failed attempts: {self.__failed_attempts}")
-
             
+            if self.__failed_attempts >= 3:
+                print("Vault locked permanently!")
+        
+        
+        # print(f"DEBUG: Is vault locked?: {self.__is_locked}")
+        # print(f"DEBUG: How many failed attempts: {self.__failed_attempts}")
+
     def lock(self):
         self.__is_locked = True
         print(f"DEBUG: Is locked?: {self.__is_locked}")
-
-
 
 
 
@@ -31,6 +30,6 @@ finster = PasswordVault("power")
 print(finster.unlock("water"))
 print(finster.unlock("gas"))
 print(finster.unlock("solid"))
-print(finster.unlock("lmao"))
+# print(finster.unlock("lmao"))
 # print(finster.unlock("power"))
-# print(finster.lock())
+print(finster.lock())
