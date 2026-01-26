7fcdca692151117877f1808575b298c6edd0f9ad
Ninny <neninny@proton.me>
2026-01-26 15:59:37 -0500

    completed the store_password method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class PasswordVault:
     def __init__(self, master_password):
         self.__master_password = master_password
-        # self.__stored_password = None
+        self.__stored_password = None
         self.__is_locked = True
         self.__failed_attempts = 0
 
@@ -16,20 +16,20 @@ class PasswordVault:
             if self.__failed_attempts >= 3:
                 print("Vault locked permanently!")
         
-        
-        # print(f"DEBUG: Is vault locked?: {self.__is_locked}")
-        # print(f"DEBUG: How many failed attempts: {self.__failed_attempts}")
-
     def lock(self):
         self.__is_locked = True
-        print(f"DEBUG: Is locked?: {self.__is_locked}")
 
+    def store_password(self, password):
+        if self.__is_locked == True:
+            print("ERROR: Your password vault is locked.")
+        else:
+            self.__stored_password = password
 
 
 finster = PasswordVault("power")
-print(finster.unlock("water"))
-print(finster.unlock("gas"))
-print(finster.unlock("solid"))
-# print(finster.unlock("lmao"))
-# print(finster.unlock("power"))
-print(finster.lock())
+
+finster.unlock("power")
+finster.store_password("fat")
+
+
+# print(finster.lock())
